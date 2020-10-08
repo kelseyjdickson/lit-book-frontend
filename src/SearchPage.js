14189d787e23b2ce15bd2a46e05d4f041ddb9bb8
kelseyjdickson <kelseymedved@gmail.com>
2020-10-08 14:05:40 -0400

    please work

---
+++ src/SearchPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const SearchPage = (props) => {
+
+  let handleChange = evt => {
+    props.changeSearchTerm(evt.target.value)
+  }
+  return (
+
+      <div className="searchBar">
+        <input class="select" type="text" placeholder="Search Lit Book by Title..." value={props.searchTerm} onChange={handleChange}aria-label="Search" />
+   </div>
+  );
+}
+
+export default SearchPage;
